a92d2e66311d51cabd2f9c4a847e09085f591f4c
harshad <harshadchavan141@gmail.com>
2023-01-20 19:33:00 +0530

    App-4 webcam photo sharer : completed 2nd page and functionality

--- App-4-Webcam-Photo-Sharer/main.py
+++ App-4-Webcam-Photo-Sharer/main.py
@@ -1,6 +1,9 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
+from datetime import datetime as dt
+from kivy.core.clipboard import Clipboard
+import webbrowser
 
 IMAGES_PATH = "files"
 Builder.load_file("frontend.kv")
@@ -20,13 +23,36 @@ class CameraScreen(Screen):
     def stop(self):
         self.ids.camera.play = False
         self.ids.start_stop.text = "Start camera"
+        self.ids.camera.texture = None
 
     def capture(self):
-        pass
+        self.file_name = dt.now().strftime("%d%m%H%M%Y") + ".png"
+        self.ids.camera.export_to_png(self.file_name)
+        # changes to the next screen
+        self.manager.current = "image_screen"
+        # at this point current screen is images creen
+        self.manager.current_screen.ids.img.source = self.file_name
 
 
 class ImageScreen(Screen):
-    pass
+    link_message = " create a link first"
+
+    def create_link(self):
+        file_path = App.get_running_app().root.ids.camera_screen.file_name
+        self.dummy_link = "http://" + file_path
+        self.ids.link_lable.text = self.dummy_link
+
+    def copy_link(self):
+        try:
+            Clipboard.copy(self.dummy_link)
+        except:
+            self.ids.link_lable.text = self.link_message
+
+    def open_link(self):
+        try:
+            webbrowser.open(self.dummy_link)
+        except:
+            self.ids.link_lable.text = self.link_message
 
 
 class MainApp(App):
